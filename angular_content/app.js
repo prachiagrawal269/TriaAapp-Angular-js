1ffdef2cbbde80dd598800a2070107701ea1c504
prachiagrawal269 <prachi.agrawal269@gmail.com>
2015-12-17 03:18:40 +0530

    Bootstrap added

--- angular_content/app.js
+++ angular_content/app.js
@@ -19,8 +19,14 @@ angular.module('flapperNews', [])
 
     if(!$scope.title || $scope.title === ''){return ;}
 
-    $scope.posts.push({title: $scope.title , upvotes: 0});
+    $scope.posts.push({
+      title: $scope.title ,
+      link: $scope.link,
+      upvotes: 0
+     });
     $scope.title = '';
+    $scope.link = '';
+
   };
 
   $scope.incrementUpvotes = function(post){
